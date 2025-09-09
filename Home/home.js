cd3f7bb8002dad8a1bf9ea5bececbc461e7b0cc2
Benjamin-Sharpe <pwaxjohn@gmail.com>
2025-09-09 10:25:09 -0400

    Update index.js

--- Home/home.js
+++ Home/home.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const burger = document.querySelector(".burger");
-    const navLinks = document.querySelector(".nav-links");
 
-    burger.addEventListener("click", function() {
-        navLinks.classList.toggle("active");
-    });
-});
